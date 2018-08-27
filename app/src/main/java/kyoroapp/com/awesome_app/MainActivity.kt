47f1ab69b0714c83746b12d138c6e0c0fa1d8694
Kyosuke Inoue <kyoro@hakamastyle.net>
2018-08-27 16:03:45 -0700

    awesome

---
+++ app/src/main/java/kyoroapp/com/awesome_app/MainActivity.kt
@@ -0,0 +1,18 @@
+package kyoroapp.com.awesome_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            label1.text = "${label1.text} Awesome!"
+        }
+    }
+}
+
